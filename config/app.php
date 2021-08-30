9b4af00d2966c43a06081c9329ad1271e4f5c1a6
jhonatan fernandez <56049236+jhonatan11530@users.noreply.github.com>
2021-08-30 09:44:58 -0500

    Se cambia la contraseña y se oculta variables del .ENV

--- config/app.php
+++ config/app.php
@@ -40,7 +40,15 @@ return [
     */
 
     'debug' => env('APP_DEBUG', false),
-
+    
+    'debug_blacklist' => [
+        '_COOKIE' => array_keys($_COOKIE),
+        '_SERVER' => array_keys($_SERVER),
+        '_ENV' => array_keys($_ENV),
+        '_POST' => [
+            'password',
+        ],      
+    ],
     /*
     |--------------------------------------------------------------------------
     | Application URL
